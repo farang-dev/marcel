90ee51a91d8e50735f68bf2a03d455c72786cc5e
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2023-08-09 23:58:45 +0900

    Remove unwanted lockfile

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,21 @@
-const path    = require("path")
-const webpack = require("webpack")
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   mode: "production",
   devtool: "source-map",
   entry: {
     application: "./app/javascript/application.js"
+  },
+  node: {
+    __dirname: false,
+    __filename: false,
+    dgram: false,
+    fs: false,
+    net: false,
+    tls: false,
+    child_process: false,
+    global: false,
   },
   output: {
     filename: "[name].js",
@@ -17,4 +27,4 @@ module.exports = {
       maxChunks: 1
     })
   ]
-}
+};
