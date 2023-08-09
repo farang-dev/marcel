781b63cd923c615ee44c0dae026527d6c197d200
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2023-08-09 12:03:00 +0900

    yml

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,4 +88,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+    # Disable Active Record entirely (optional, if you don't use it)
+    config.active_record.enabled = false
 end
