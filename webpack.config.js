d9a1393dd16f707382c2ed3363c937de14e51298
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2023-08-10 14:29:34 +0900

    modified database.yml

--- webpack.config.js
+++ webpack.config.js
@@ -9,5 +9,11 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve(__dirname, "public/packs"),
     publicPath: "/packs/"
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+    alias: {
+      controllers: path.resolve(__dirname, 'app/controllers'),
+    },
   }
 };
