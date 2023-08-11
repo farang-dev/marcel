df50e18c2f285525538b5fc7285f2d988f48aeb0
Masafumi Nozawa <masafumi.nozawa.q@gmail.com>
2023-08-12 02:06:06 +0900

    asset precompile fixed

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -5,7 +5,7 @@ Rails.application.config.assets.version = "1.0"
 Rails.application.config.assets.paths << Rails.root.join("node_modules","assets")
 
 # Add additional assets to the asset load path.
-# Rails.application.config.assets.paths << Emoji.images_path  c
+# Rails.application.config.assets.paths << Emoji.images_path
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
